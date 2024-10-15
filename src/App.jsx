b1a97efbdc4fc316a34acca05f5178509dcea424
jovanlekic12 <lekicjovan1212@gmail.com>
2024-10-15 19:21:09 +0200

    add fetching

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,48 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [error, setError] = useState();
   const url = "https://opentdb.com/api.php?";
+
   const categories = {
     Sports: 21,
     GeneralKnowledge: 9,
     VideoGames: 27,
     Mythology: 20,
   };
-  console.log(categories["Sports"]);
+
+  const [amount, setAmount] = useState(10);
+  const [category, setCategory] = useState(categories["GeneralKnowledge"]);
+  const [difficulty, setDifficulty] = useState("easy");
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        url + `amount=${amount}&category=${category}&difficulty=${difficulty}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetchData();
+  }
+
   return (
     <main className="main__container">
-      <form action="" className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h1 className="title">Quiz setup</h1>
         <div className="input__container">
           <label htmlFor="numberOfQuestions">Number of questions:</label>
           <input
             type="number"
             className="questions__numbers"
-            defaultValue={10}
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
           />
         </div>
         <div className="input__container">
